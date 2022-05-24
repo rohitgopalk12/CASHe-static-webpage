c0c7c069793e017ea546950c2d67038703e2d8bd
Rohit Gopal <rohitgopalraok.it@gmail.com>
2022-05-24 20:15:29 +0530

    developers credit added

--- js/casheLearnings.js
+++ js/casheLearnings.js
@@ -17,53 +17,53 @@ casheFiles.set('vid6','https://www.youtube.com/embed/z_zj2OmTRJk');
                                 /* word documents links */
                                 //=========================
 
-casheFiles.set('word-doc-1','assets/3_reasons_BNPL_is_a_hit_among_young_shoppers_this_festive_season.pdf');
+casheFiles.set('word-doc-1','assets/3_reasons_bnpl_is_a_hit_among_young_shoppers_this_festive_season.pdf');
 casheFiles.set('word-doc-2','assets/5_apps_to_consider_for_micro_loans_this_festive_season.pdf');
-casheFiles.set('word-doc-3','assets/5_Tech-Based_Platforms_Driving_Credit_Literacy.pdf');
-casheFiles.set('word-doc-4','assets/Banking_2_dot_0.pdf');
-casheFiles.set('word-doc-5','assets/BHANIX_FINANCE.pdf');
+casheFiles.set('word-doc-3','assets/5_tech-based_platforms_driving_credit_literacy.pdf');
+casheFiles.set('word-doc-4','assets/banking_2_dot_0.pdf');
+casheFiles.set('word-doc-5','assets/bhanix_finance.pdf');
 
-casheFiles.set('word-doc-6','assets/BNPL_lights_up_India’s_ecommerce_sales_this_Diwali.pdf');
-casheFiles.set('word-doc-7','assets/Buy_now_pay_later_set_to_surge_over_ten_fold_in_India.pdf');
-casheFiles.set('word-doc-8','assets/Buy_now,pay_later_set_to_surge_over_ten-fold_in_India-reuters.pdf');
-casheFiles.set('word-doc-9','assets/Buy-Now,_Pay-Later_Loans_Help_Fuel_India’s_Festive_Recovery.pdf');
-casheFiles.set('word-doc-10','assets/CASHe_appoints_veteran_banker_Jogindar_Rana_to_Top_Role.pdf');
+casheFiles.set('word-doc-6','assets/bnpl_lights_up_india’s_ecommerce_sales_this_diwali.pdf');
+casheFiles.set('word-doc-7','assets/buy_now_pay_later_set_to_surge_over_ten_fold_in_india.pdf');
+casheFiles.set('word-doc-8','assets/buy_now,pay_later_set_to_surge_over_ten-fold_in_india-reuters.pdf');
+casheFiles.set('word-doc-9','assets/buy_now,_pay_later_loans_help_fuel_india’s_festive_recovery.pdf');
+casheFiles.set('word-doc-10','assets/cashe_appoints_veteran_banker_jogindar_rana_to_top_role.pdf');
 
-casheFiles.set('word-doc-11','assets/CASHe_product_pipeline.pdf');
-casheFiles.set('word-doc-12','assets/CASHe_Raises_Rs_140_Cr_V_Raman_Kumar_Talks_About_Growth_Ahead.pdf');
-casheFiles.set('word-doc-13','assets/CASHe_roadmap.pdf');
-casheFiles.set('word-doc-14','assets/CASHe.pdf');
-casheFiles.set('word-doc-15','assets/CXO_Exclusive-Building_a_Digital-first_Customer_Acquisition_and_Collection_Process.pdf');
+casheFiles.set('word-doc-11','assets/cashe_product_pipeline.pdf');
+casheFiles.set('word-doc-12','assets/cashe_raises_rs_140_cr_v_raman_kumar_talks_about_growth_ahead.pdf');
+casheFiles.set('word-doc-13','assets/cashe_roadmap.pdf');
+casheFiles.set('word-doc-14','assets/cashe.pdf');
+casheFiles.set('word-doc-15','assets/cxo_exclusive-building_a_digital-first_customer_acquisition_and_collection_process.pdf');
 
-casheFiles.set('word-doc-16','assets/Exploring_AI-Driven_Financial_Inclusion_with_Yashoraj_Tyagi,_CTO_of_CASHe.pdf');
-casheFiles.set('word-doc-17','assets/Fintech_Platform_raises_$19_million_from_its_Singaporean_parent_TSLC.pdf');
-casheFiles.set('word-doc-18','assets/How_to_safeguard_yourself_from_financial_frauds.pdf');
-casheFiles.set('word-doc-19','assets/Is_Buy_Now,_Pay_Later_A_Good_Idea_For_Consumers.pdf');
-casheFiles.set('word-doc-20','assets/Pandemic_Fuels_Tech_Inclusion_in_the_Financial_Sector.pdf');
+casheFiles.set('word-doc-16','assets/exploring_ai-driven_financial_inclusion_with_yashoraj_tyagi,_ct_of_cashe.pdf');
+casheFiles.set('word-doc-17','assets/fintech_platform_raises_$19_million_from_its_singaporean_parent_tslc.pdf');
+casheFiles.set('word-doc-18','assets/how_to_safeguard_yourself_from_financial_frauds.pdf');
+casheFiles.set('word-doc-19','assets/is_buy_now,_pay_later_a_good_idea_for_consumers.pdf');
+casheFiles.set('word-doc-20','assets/pandemic_fuels_tech_inclusion_in_the_financial_sector.pdf');
 
-casheFiles.set('word-doc-21','assets/Personal_Lending_Startup_CASHe_secures_INR_140_Crores_Equity_funding.pdf');
-casheFiles.set('word-doc-22','assets/Personal_Loans_for_young_salaried_millennials_across_India.pdf');
-casheFiles.set('word-doc-23','assets/Sept_records_35_percent_rise_in_BNPL_loan_applications.pdf');
-casheFiles.set('word-doc-24','assets/The_evolution_of_the_digital_lending_sector_in_India.pdf');
-casheFiles.set('word-doc-25','assets/Top_apps_that_help_you_take_an_instant_personal_loan.pdf');
-casheFiles.set('word-doc-26','assets/With_10x_growth_for_BNPL_products_during_sales.pdf');
+casheFiles.set('word-doc-21','assets/personal_lending_startup_cashe_secures_inr_140_crores_equity_funding.pdf');
+casheFiles.set('word-doc-22','assets/personal_loans_for_young_salaried_millennials_across_india.pdf');
+casheFiles.set('word-doc-23','assets/sept_records_35_percent_rise_in_bnpl_loan_applications.pdf');
+casheFiles.set('word-doc-24','assets/the_evolution_of_the_digital_lending_sector_in_india.pdf');
+casheFiles.set('word-doc-25','assets/top_apps_that_help_you_take_an_instant_personal_loan.pdf');
+casheFiles.set('word-doc-26','assets/with_10x_growth_for_bnpl_products_during_sales.pdf');
 
 //====================================================================================
                                         /* pdf docs */
                                         //============
 
-casheFiles.set('pdf-doc-1','assets/BHANIX-and-CASHe.pdf');
-casheFiles.set('pdf-doc-2','assets/BUY-NOW-PAY-LATER.pdf');
-casheFiles.set('pdf-doc-3','assets/FinTech-Financial-Sector.pdf');
+casheFiles.set('pdf-doc-1','assets/bhanix_and_cashe.pdf');
+casheFiles.set('pdf-doc-2','assets/buy_now_pay_later.pdf');
+casheFiles.set('pdf-doc-3','assets/fintech_financial_sector.pdf');
 
 //====================================================================================
 
                                         /* ppt files */
                                         //================
 
-casheFiles.set('ppt-doc-1','assets/BHANIX-and-CASHe.pdf');
-casheFiles.set('ppt-doc-2','assets/BUY-NOW-PAY-LATER.pdf');
-casheFiles.set('ppt-doc-3','assets/FinTech-Financial-Sector.pdf');
+casheFiles.set('ppt-doc-1','assets/bhanix_and_cashe.pdf');
+casheFiles.set('ppt-doc-2','assets//buy_now_pay_later.pdf');
+casheFiles.set('ppt-doc-3','assets/fintech_financial_sector.pdf');
 
 //====================================================================================
 
